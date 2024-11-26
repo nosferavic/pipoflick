3c3111625e1ebbd63c2acd9f748aca403dd8f272
victor viana <victor.deviana@outlook.com>
2024-11-26 00:51:45 -0300

    buttonSlide off

--- catalog/engine/script.js
+++ catalog/engine/script.js
@@ -19,12 +19,22 @@ const button1 = document.querySelector(".button-slide .part1");
 const button2 = document.querySelector(".button-slide .part2");
 const movieSlides = document.querySelectorAll(".movies");
 
+
+
 button2.addEventListener("click", () => {
+  if(button1.classList.contains('off')){
+    button1.classList.remove('off')
+  }
+  button2.classList.add('off')
   movieSlides.forEach((slide) => slide.classList.remove("active"));
   movieSlides[1].classList.add("active");
 });
 
 button1.addEventListener("click", () => {
+  if(button2.classList.contains('off')){
+    button2.classList.remove('off')
+  }
+  button1.classList.add('off')
   movieSlides.forEach((slide) => slide.classList.remove("active"));
   movieSlides[0].classList.add("active");
 });
