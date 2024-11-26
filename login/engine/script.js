b3eeed5c429832a76b9c3e9ac5ffad65c9b4ae57
victor viana <victor.deviana@outlook.com>
2024-11-26 01:41:25 -0300

    loginFuncional

---
+++ login/engine/script.js
@@ -0,0 +1,13 @@
+const check = document.getElementById("check");
+const password = document.querySelector(".senha");
+
+function alterType() {
+  if (check.checked) {
+    password.setAttribute("type", "text");
+  } else {
+    password.setAttribute("type", "password");
+  }
+}
+
+check.addEventListener("change", alterType);
+
